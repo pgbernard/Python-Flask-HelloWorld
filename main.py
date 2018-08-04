cd180dfb9ccda378dc1fc7cbd7d2e15203d7fd04
pgbernard <pgbernard@users.noreply.github.com>
2018-08-04 15:52:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 try:
-  import pandas as pd
+  print('importing PANDAS')
+  #import pandas as pd
   
 from flask import Flask
 app = Flask(__name__)
@@ -12,7 +13,8 @@ def hello_world():
     data = np.array([['','Col1','Col2'], ['Row1',1,2], ['Row2',3,4]])              
     print(pd.DataFrame(data=data[1:,1:], index=data[1:,0], columns=data[0,1:]))
   
-  return 'Hey its Python Flask application!'
+  except:
+    return 'Hey its Python Flask application!'
 
 if __name__ == '__main__':
   app.run()
