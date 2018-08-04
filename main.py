b4f59e32b9e2d1158ad68ce1c3712b915b113653
pgbernard <pgbernard@users.noreply.github.com>
2018-08-04 15:51:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import pandas as pd
+try:
+  import pandas as pd
+  
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello_world():
-  data = np.array([['','Col1','Col2'], ['Row1',1,2], ['Row2',3,4]])              
-  print(pd.DataFrame(data=data[1:,1:], index=data[1:,0], columns=data[0,1:]))
+  try:
+  
+    data = np.array([['','Col1','Col2'], ['Row1',1,2], ['Row2',3,4]])              
+    print(pd.DataFrame(data=data[1:,1:], index=data[1:,0], columns=data[0,1:]))
   
   return 'Hey its Python Flask application!'
 
