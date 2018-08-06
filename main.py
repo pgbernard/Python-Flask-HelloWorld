da7dcb5dfc70f0674332e87d557d5406583aecbd
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 14:00:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 def test():
     arr = np.array([2,3,1,0])
     arrLen = len(arr)
-    return 'len function used'
+    return arrLen
+    #return 'len function used'
     #return len(np.array([2,3,1,0]))
     #d = {'col1': [1, 2], 'col2': [3, 5]}
     #df = pd.DataFrame(data=d)
