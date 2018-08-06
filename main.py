d0c1fb7c6a5431fb5d2a0f010a2399534bfb45b9
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 11:06:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 sitepackage = "D:\home\site\wwwroot\env\Lib\site-packages"
 sys.path.append(sitepackage)
 
-import numpy as np
+import pandas as pd
 
 from flask import Flask
 app = Flask(__name__)
@@ -17,11 +17,11 @@ app = Flask(__name__)
 def hello_world():
   
   # --------------test for checking Pandas ---------------- # 
-  #d = {'col1': [1, 2], 'col2': [3, 4]}
-  #df = pd.DataFrame(data=d)
-  #df
-  a = np.array([1, 2, 3])
-  print(a)
+  d = {'col1': [1, 2], 'col2': [3, 4]}
+  df = pd.DataFrame(data=d)
+  df
+  #a = np.array([1, 2, 3])
+  #print(a)
   # ------------------------------------------------------- #
   
   return 'Hey its Python Flask application!'
