5357b87e26c61efabb832fdca27a642780cdd3b7
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 13:53:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def test():
     arr = np.array([2,3,1,0])
     arrLength = len(arr)
-    return 'RESCUE ME'
+    return arrLength
     #return len(np.array([2,3,1,0]))
     #d = {'col1': [1, 2], 'col2': [3, 5]}
     #df = pd.DataFrame(data=d)
