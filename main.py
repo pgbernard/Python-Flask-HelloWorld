cee4c049f7ddf89e6802f7526eff2bf7e18d8d33
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 13:35:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def test():
     d = {'col1': [1, 2], 'col2': [3, 5]}
     df = pd.DataFrame(data=d)
     #x = len(np.array([2,3,1,0]))
-    return 'numpy ignored'
+    return len(np.array([2,3,1,0]))
 
 if __name__ == '__main__':
     app.run()
