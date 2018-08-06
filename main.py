187ed0dc7b22180d31c16edab0d9e7dcd373a075
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 11:15:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-
 import os
 import sys
+import pandas as pd
 
 sitepackage = "D:\home\site\wwwroot\env\Lib\site-packages"
 sys.path.append(sitepackage)
 
-import pandas as pd
 
 from flask import Flask
 app = Flask(__name__)
-# --------------------------------------------------------- #
-
 @app.route('/')
 
+def test():
+  
+    # --------------test for checking Pandas ---------------- # 
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    df.head
+    # ------------------------------------------------------- #
 
-def hello_world():
-  
-  # --------------test for checking Pandas ---------------- # 
-  d = {'col1': [1, 2], 'col2': [3, 4]}
-  df = pd.DataFrame(data=d)
-  df
-  #a = np.array([1, 2, 3])
-  #print(a)
-  # ------------------------------------------------------- #
-  
-  return 'Hey its Python Flask application!'
+    return 'Python Flask works.'
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
