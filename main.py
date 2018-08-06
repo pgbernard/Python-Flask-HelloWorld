452ff014f4d4e8bfecc75357f7e7661e7d205286
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 16:16:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import os
-import sys
 import pandas as pd
-import scipy as sp
 import numpy as np
+import json
+import datetime
+import time
+import math
+                                                                                                            
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import minmax_scale
+from sklearn import preprocessing
+from datetime import timedelta, date
 
 from flask import Flask
 app = Flask(__name__)
@@ -10,7 +16,26 @@ app = Flask(__name__)
 @app.route('/')
 
 def main():
-    return 'ok'
+    # Period for retrieving historical data = N previous days from today
+    n_days = 30
+
+    # Max Percentage of Keywords to remove
+    # Number of the best interests keyword to subset to create separated adsets
+    margin = .25
+    top = 3
+
+    # Get the START and END date for INSIGHTS DATA over 'n_days' ------------- # 
+
+    now = datetime.datetime.now()
+
+    year, month, day = now.year, now.month, now.day
+    period = now - datetime.timedelta(days = n_days)
+    year_p, month_p, day_p = period.year, period.month, period.day
+
+    end_dt = date(year, month, day)
+    start_dt = date(year_p, month_p, day_p)
+
+    return now
 
 if __name__ == '__main__':
     main()
