ae5d6683c20ab4c6962fe791a54b55080366496b
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 13:36:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def test():
     # --------------test for checking Pandas ---------------- # 
     d = {'col1': [1, 2], 'col2': [3, 5]}
     df = pd.DataFrame(data=d)
-    #x = len(np.array([2,3,1,0]))
-    return len(np.array([2,3,1,0]))
+    x = len(np.array([2,3,1,0]))
+    return x
 
 if __name__ == '__main__':
     app.run()
