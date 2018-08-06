aa1be82214417d491bfcb6138d05cb1beaa6f093
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 13:10:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 
 from flask import Flask
 app = Flask(__name__)
@@ -12,12 +13,13 @@ def test():
     try:
         d = {'col1': [1, 2], 'col2': [3, 5]}
         df = pd.DataFrame(data=d)
-        var = 'pandas is working'
+        x = len(np.array([2,3,1,0]))
+        #var = 'pandas is working'
     except:
         var = 'error'
     #print(df)
     # ------------------------------------------------------- #
-    return var
+    return x
 
 if __name__ == '__main__':
     app.run()
