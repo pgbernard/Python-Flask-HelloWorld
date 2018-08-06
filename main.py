72284d79b86b17436c8fe8221ccebe897ea3f1ba
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 13:38:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 
 def test():
-    # --------------test for checking Pandas ---------------- # 
     d = {'col1': [1, 2], 'col2': [3, 5]}
     df = pd.DataFrame(data=d)
     x = len(np.array([2,3,1,0]))
