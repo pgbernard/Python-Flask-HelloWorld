3625774261ec51870995e53783e869fe6729040a
pgbernard <pgbernard@users.noreply.github.com>
2018-08-06 10:02:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-#import pandas as pd
+import pandas as pd
 import numpy as np
 
 from flask import Flask
@@ -9,7 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-
+  
+  # --------------test for checking Pandas ---------------- # 
+  d = {'col1': [1, 2], 'col2': [3, 4]}
+  df = pd.DataFrame(data=d)
+  df
+  
   return 'Hey its Python Flask application!'
 
 if __name__ == '__main__':
