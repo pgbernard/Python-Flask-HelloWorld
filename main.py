b10a7125a1d0ee6bc8da65e8135216d6d96590f8
pgbernard <pgbernard@users.noreply.github.com>
2018-08-09 12:40:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 #import json
 #import datetime
 #import time
@@ -18,28 +18,6 @@ app = Flask(__name__)
 def main():
     return 'ok'
 
-def test():
-    # Period for retrieving historical data = N previous days from today
-    n_days = 30
-
-    # Max Percentage of Keywords to remove
-    # Number of the best interests keyword to subset to create separated adsets
-    margin = .25
-    top = 3
-
-    # Get the START and END date for INSIGHTS DATA over 'n_days' ------------- # 
-
-    #now = datetime.datetime.now()
-
-    #year, month, day = now.year, now.month, now.day
-    #period = now - datetime.timedelta(days = n_days)
-    #year_p, month_p, day_p = period.year, period.month, period.day
-
-    #end_dt = date(year, month, day)
-    #start_dt = date(year_p, month_p, day_p)
-    return n_days / top
-
-#main()
 
 if __name__ == '__main__': 
     main()
