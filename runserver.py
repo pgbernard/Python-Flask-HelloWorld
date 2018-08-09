3ebd64e737ab67beac6d3cd21a59aca72f8f31c0
pgbernard <pgbernard@users.noreply.github.com>
2018-08-09 14:51:38 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,10 +1,9 @@
-#!D:\home\site\wwwroot python36
-from flask import Flask  
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():  
-    return "Hello World!"
+def hello():
+    return "Hello Flask!"
 
-if __name__ == "__main__":  
-    app.run()
+if __name__ == "__main__":
+app.run()
