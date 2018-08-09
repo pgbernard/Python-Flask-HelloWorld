afd2409dae22a78c43c2cbaf98c7dbbb2e81de7e
pgbernard <pgbernard@users.noreply.github.com>
2018-08-09 16:26:28 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route("/")
 
 def main():
-    age = {"Sam": 20, "Ken": 30, "Grace": 23, "Peter": 29}
-    return render_template("index.html")
+	age = {"Sam": 20, "Ken": 30, "Grace": 23, "Peter": 29}
+	return render_template("index.html", ages = age)
 
 if __name__ == "__main__":
     app.run()
