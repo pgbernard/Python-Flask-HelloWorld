1caf28e307542c148602d5819c48b42cd7b96dae
pgbernard <pgbernard@users.noreply.github.com>
2018-08-09 16:14:26 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, request
-
+from flask import Flask,render_template, request
 import pandas as pd
 import numpy as np
-
 app = Flask(__name__)
 
 @app.route("/")
 
-def hello():
-    age = {"Sam": 20, "Ken": 30, "Grace": 23, "Peter": 29}
-    return render_template("test.html", ages=age)
+def index():
+	age = {"Sam": 20, "Ken": 30, "Grace": 23, "Peter": 29}
+	return render_template("index.html", ages=age)
 
 if __name__ == "__main__":
     app.run()
