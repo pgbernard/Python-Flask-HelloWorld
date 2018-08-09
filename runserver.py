31c6de7249b765e9eaddfafe69e86f29167759c2
pgbernard <pgbernard@users.noreply.github.com>
2018-08-09 14:48:01 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,15 +1,4 @@
-#import pandas as pd
-#import numpy as np
-#import json
-#import datetime
-#import time
-#import math
-                                                                                                            
-#from sklearn.preprocessing import MinMaxScaler
-#from sklearn.preprocessing import minmax_scale
-#from sklearn import preprocessing
-#from datetime import timedelta, date
-import os, sys
+#!/home/python364x64 python
 from flask import Flask  
 app = Flask(__name__)
 
