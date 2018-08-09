c7342c4c7aa78cf16b425ed631d2487ee812cfa3
pgbernard <pgbernard@users.noreply.github.com>
2018-08-09 15:49:18 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,11 +1,16 @@
-import pandas
 
 from flask import Flask
+
+import pandas
+
 app = Flask(__name__)
 
 @app.route("/")
+
 def hello():
-    return "Hello Flask!"
+    df_ = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
+    number = 123456789
+    return render_template('test.html', number=str(number), data = df_.to_html())
 
 if __name__ == "__main__":
     app.run()
